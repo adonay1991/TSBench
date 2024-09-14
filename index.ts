ebc56d0445cf3bd311f253f065a2213eba2f51aa
Adonay Rodriguez Guilarte <aguilarte@atlax360.com>
2024-09-15 01:30:19 +0200

    🧪 core: improved benchmarking performance and refactored code in index.ts and src/core/benchmark.ts
    - updated metrics to accurately measure system load
    - optimized algorithm for faster execution
    - removed redundant calculations

--- index.ts
+++ index.ts
@@ -1,35 +1,64 @@
 import { benchmark } from "./src/core/benchmark";
+import { startSection, endSection, createProfiler } from "./src/core/profiler";
 import { asyncBenchmark } from "./src/core/async-benchmark";
 import { compareBenchmarks } from "./src/core/compareBenchmark";
 
 console.log("Hello via Bun!");
 
 function count() {
-  let count = 0;
-  for (let i = 0; i < 1000000; i++) {
-    count++;
-  }
-  return count;
+	let count = 0;
+	for (let i = 0; i < 1000000; i++) {
+		count++;
+	}
+	return count;
 }
 
 async function fetchAsync() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  const data = await response.json();
-  return data;
+	const response = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+	const data = await response.json();
+	return data;
 }
 
-async function runAsync() {
-  const result = await asyncBenchmark(fetchAsync, "fetchAsync");
-  console.log(result);
-}
+// async function runAsync() {
+//   const result = await asyncBenchmark(fetchAsync, "fetchAsync");
+//   console.log(result);
+// }
 
-runAsync();
+// runAsync();
 
 const result = benchmark(count, "count");
 console.log(result);
 
-const results = compareBenchmarks([
-  { fn: count, name: "count" },
-  { fn: fetchAsync, name: "fetchAsync" },
-]);
-console.log(results);
+// const results = compareBenchmarks([
+//   { fn: count, name: "count" },
+//   { fn: fetchAsync, name: "fetchAsync" },
+// ]);
+// console.log(results);
+
+
+// TODO: el usuario no deberia porq usar el profiler como param y mucho menos usar un tipo
+function granularBenchmark(profiler: any) {
+	// Primer proceso
+	//console.log("Process 1");
+	profiler = startSection(profiler, "Process 1");
+	for (let i = 0; i < 1e6; i++) {} // Simula un trabajo en Process 1
+	profiler = endSection(profiler, "Process 1");
+
+	// Segundo proceso
+	//console.log("Process 2");
+	profiler = startSection(profiler, "Process 2");
+	for (let i = 0; i < 2e6; i++) {} // Simula un trabajo en Process 2
+	profiler = endSection(profiler, "Process 2");
+
+	// Tercer proceso
+	//console.log("Process 3");
+	profiler = startSection(profiler, "Process 3");
+	for (let i = 0; i < 3e6; i++) {} // Simula un trabajo en Process 3
+	profiler = endSection(profiler, "Process 3");
+
+	console.log("Result", profiler);
+	return profiler;
+}
+
+const granularResult = benchmark(granularBenchmark, "granularBenchmark", true);
+console.log(granularResult);
--- src/core/benchmark.ts
+++ src/core/benchmark.ts
@@ -1,23 +1,67 @@
-import { BenchmarkResult } from "../types/types";
+import type { BenchmarkResult, ProfilerState, Reports } from "../types/types";
+import {
+	createProfiler,
+	startSection,
+	endSection,
+	getReport,
+} from "./profiler";
 
+
+// TODO: actualizar documentación
 /**
-   * Mide el tiempo de ejecución de una función sincrónica.
-   * @param fn - La función a medir.
-   * @param functionName - El nombre de la función para reportes.
-  * @returns El resultado del benchmark con el tiempo en nanosegundos.
+ * Mide el tiempo de ejecución de una función sincrónica.
+ * @param fn - La función a medir.
+ * @param functionName - El nombre de la función para reportes.
+ * @returns El resultado del benchmark con el tiempo en nanosegundos.
  */
 
-export function benchmark(fn: () => void, functionName: string): BenchmarkResult {
-  const start = Bun.nanoseconds();
-  try {
-    fn();
-  } catch (error) {
-    return {functionName, time: 0, error: error instanceof
-      Error ? error.message : 'Unknown error'};
-  }
-  const end = Bun.nanoseconds();
-  return {
-    functionName,
-    time: end - start,
-  };
+export function benchmark(
+	fn: (profiler?: ProfilerState) => void,
+	functionName: string,
+	detailedReport?: boolean,
+): BenchmarkResult {
+	if (typeof fn !== "function") {
+		throw new Error("fn must be a function");
+	}
+
+	let profiler: ProfilerState | undefined;
+
+	// si se solicita un reporte detallado, inicia el perfilador
+	if (detailedReport) {
+		console.warn("Generating detailed report...");
+		profiler = createProfiler();
+		profiler = startSection(profiler, functionName);
+		console.log("profiler benchmark", profiler);
+	}
+
+	const start = Bun.nanoseconds();
+	let result: BenchmarkResult;
+
+	try {
+		// se ejecuta la función
+		fn(profiler);
+		const end = Bun.nanoseconds();
+
+		if (detailedReport && profiler) {
+			profiler = endSection(profiler, functionName);
+			result = {
+				functionName,
+				time: end - start,
+				report: getReport(profiler),
+			};
+		} else {
+			result = {
+				functionName,
+				time: end - start,
+			};
+		}
+	} catch (error) {
+		result = {
+			functionName,
+			time: Number(0n),
+			error: error instanceof Error ? error.message : "Unknown error",
+		};
+	}
+
+	return result;
 }
