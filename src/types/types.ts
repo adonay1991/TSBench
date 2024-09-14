4c5d26b89288ea35eaf260b7a0275e8472ffc5cf
Adonay Rodriguez Guilarte <aguilarte@atlax360.com>
2024-09-15 01:29:44 +0200

    feat(types): improve type definitions
    - update types to match new api requirements
    - add type checking for better code reliability
    - BREAKING CHANGE: updated types may require updates in other parts of the app

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,16 @@
 export type BenchmarkResult = {
-  functionName: string;
-  time?: number;
-  error?: string;
+	functionName: string;
+	time?: number;
+	error?: string;
+	detailedReport?: boolean;
+	report?: { [key: string]: any };
 };
 
+export type Reports = {
+	fn?: () => void;
+};
 
+export type ProfilerState = {
+	startTimes?: { [key: string]: number | bigint | undefined }; // Usamos bigint para nanosegundos
+	sections: { [key: string]: number | bigint }; // Acumulación de tiempos en nanosegundos
+};
